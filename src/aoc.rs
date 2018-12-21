f19778524d913c62beeca24d71bef6318b20ab25
Melvillian <melvillevt@gmail.com>
2018-12-21 05:19:14 -0800

    complete day 1 and first part of day 2

--- src/aoc.rs
+++ src/aoc.rs
@@ -1,12 +1,108 @@
-pub fn aoc_1() {
+use std::fs;
+use std::collections::HashSet;
+use std::collections::HashMap;
 
+/// Sums the numeric values on each line in the input
+pub fn aoc_1_1() {
+    let frequencies = get_input_file_as_string("aoc_1_input.txt");
+
+    let mut sum : i32 = 0;
+    for line in frequencies.lines() {
+        let (sign, num) = line.split_at(1);
+        let num : i32 = num.parse().unwrap();
+        match sign {
+            "+" => sum+= num,
+            "-" => sum-= num,
+            _ => panic!("unknown symbol {}", sign),
+        }
+    }
+
+    println!("aoc_1_1: {}", sum);
 }
 
-pub fn aoc_2() {
+/// looks for duplicate frequencies in the input list
+pub fn aoc_1_2() {
+    let mut map = HashSet::new();
 
+    let frequencies = get_input_file_as_string("aoc_1_input.txt");
+
+    let mut sum : i32 = 0;
+
+    'outer: loop {
+        'inner: for line in frequencies.lines() {
+            let is_duplicate = map.insert(sum);
+
+            if !is_duplicate {
+                break 'outer;
+            }
+
+            let (sign, num) = line.split_at(1);
+            let num : i32 = num.parse().unwrap();
+            match sign {
+                "+" => sum+= num,
+                "-" => sum-= num,
+                _ => panic!("unknown symbol {}", sign),
+            }
+        }
+    }
+
+    println!("aoc_1_2: {}", sum);
 }
 
-pub fn aoc_3() {
+/* For example, if you see the following box IDs:
+
+abcdef contains no letters that appear exactly two or three times.
+bababc contains two a and three b, so it counts for both.
+abbcde contains two b, but no letter appears exactly three times.
+abcccd contains three c, but no letter appears exactly two times.
+aabcdd contains two a and two d, but it only counts once.
+abcdee contains two e.
+ababab contains three a and three b, but it only counts once.*/
+pub fn aoc_2_1() {
+    let box_ids = get_input_file_as_string("aoc_2_input.txt");
+
+    let mut exactly_2_num : u32 = 0;
+    let mut exactly_3_num : u32 = 0;
+
+    for bar_code in box_ids.lines() {
+
+        let mut map = HashMap::new();
+
+        for letter in bar_code.split("") {
+            if letter == "" {
+                // `.split` puts a "" at the beginning and end, so we want to skip that
+                continue;
+            }
+            let count = map.get(letter);
+
+            match count {
+                Some(count) => map.insert(letter, count + 1),
+                None => map.insert(letter, 1),
+            };
+        }
+
+        let mut three_seen = false;
+        let mut two_seen = false;
+        for (_, val) in map.iter() {
+            if !three_seen && val == &3 {
+                three_seen = true;
+                exactly_3_num+= 1;
+            }
+            if !two_seen && val == &2 {
+                two_seen = true;
+                exactly_2_num+= 1;
+            }
+            if two_seen && three_seen {
+                break;
+            }
+        }
+    }
+
+    let checksum = exactly_2_num * exactly_3_num;
+    println!("aoc_2_1: checksum: {}, exactly 2: {}, exactly 3: {}", checksum, exactly_2_num, exactly_3_num);
+}
+
+pub fn aoc_2_2() {
 
 }
 
@@ -20,4 +116,8 @@ pub fn aoc_5() {
 
 pub fn aoc_6() {
 
+}
+
+fn get_input_file_as_string(filename: &str) -> String {
+    fs::read_to_string(filename).unwrap()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 mod aoc;
 
 fn main() {
-    crate::aoc::clarinet();
+    crate::aoc::aoc_1_1();
+    crate::aoc::aoc_1_2();
+    crate::aoc::aoc_2_1();
+    crate::aoc::aoc_2_2();
+    crate::aoc::aoc_4();
+    crate::aoc::aoc_5();
+    crate::aoc::aoc_6();
 }
